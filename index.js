101c90cd7d7fb70aef63d3845b96dcf8c13c56d5
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-10-20 23:20:22 +0200

    add search feature

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const pagination = document.querySelector('[data-js="pagination"]');
 // States
 let maxPage = 1;
 let page = 1;
-const searchQuery = "";
+let searchQuery = "";
+let searchPages = 1;
 
 // fetch
 const url = "https://rickandmortyapi.com/api/character/";
@@ -23,9 +24,10 @@ export async function fetchCharacters(anyUrl) {
 
     // ------------  update states ---------------
     maxPage = data.info.pages;
+    searchPages = data.info.pages;
     console.log({ maxPage });
     console.log(data);
-updatePageNumber();
+    updatePageNumber();
     data.results.map((character) => {
       // console.log("All character data: ", character);
 
@@ -64,7 +66,7 @@ updatePageNumber();
         </div>
       </li>
       `;
-      
+
       cardContainer.insertAdjacentHTML("beforeEnd", createCard);
     });
   } catch (error) {
@@ -82,26 +84,98 @@ updatePageNumber();
 //     "prev": "https://rickandmortyapi.com/api/character/?page=9"
 //   }
 
-nextButton.addEventListener("click", () =>{
-  page === maxPage ? page = 1 : page++
-updatePageNumber()
-  const costumUrl = `${url}?page=${page}`
-  console.log({ costumUrl });
-  cardContainer.innerHTML = "";
-  fetchCharacters(costumUrl);
-  console.log({page});}
-);
-prevButton.addEventListener("click", () =>{
-  page <= 1 ? page = 1 : page--
+nextButton.addEventListener("click", () => {
+  page === maxPage ? (page = 1) : page++;
+  updatePageNumber();
   const costumUrl = `${url}?page=${page}`;
   console.log({ costumUrl });
   cardContainer.innerHTML = "";
   fetchCharacters(costumUrl);
-  console.log({page});}
-);
+  console.log({ page });
+});
+prevButton.addEventListener("click", () => {
+  page <= 1 ? (page = 1) : page--;
+  const costumUrl = `${url}?page=${page}`;
+  console.log({ costumUrl });
+  cardContainer.innerHTML = "";
+  fetchCharacters(costumUrl);
+  console.log({ page });
+});
 function updatePageNumber() {
-  pagination.innerHTML = ''
-  pagination.innerHTML = `${page} / ${maxPage}`
+  pagination.innerHTML = "";
+  pagination.innerHTML = `${page} / ${maxPage}`;
+}
+
+// search bar
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const inputQuery = Object.fromEntries(formData);
+  console.log(inputQuery.query);
+  searchQuery = inputQuery.query;
+  search(searchQuery)
+  // pages >= 1 ? (page = pages) : page--;
+  // const costumUrl = `${url}?name=${inputQuery.query}`;
+  // console.log({ costumUrl });
+  // cardContainer.innerHTML = "";
+  // fetchCharacters(costumUrl);
+});
+async function search(searchQueryState) {
+  // update query state
+
+  const costumUrl = `${url}?name=${searchQueryState}`;
+  try {
+    const response = await fetch(costumUrl);
+    const data = await response.json();
+    console.log({data});
+
+    if (data.error) {
+      alert('noooooooooo')
+    }
+    data.results.map((character) => {
+      // console.log("All character data: ", character);
+
+      // const characterPicture = character.image;
+      // const characterName = character.name;
+      // const characterStatus = character.status;
+      // const characterType = character.type;
+      // const characterOccurrences = character.episode.length;
+
+      // console.log(characterPicture);
+      // console.log(characterName);
+      // console.log(characterStatus);
+      // console.log(characterType);
+      // console.log(characterOccurrences);
+
+      const createCard = `
+      <li class="card">
+        <div class="card__image-container">
+          <img
+            class="card__image"
+            src=${character.image}
+            alt=${character.name}
+          />
+          <div class="card__image-gradient"></div>
+        </div>
+        <div class="card__content">
+          <h2 class="card__title">${character.name}</h2>
+          <dl class="card__info">
+            <dt class="card__info-title">Status</dt>
+            <dd class="card__info-description">${character.status}</dd>
+            <dt class="card__info-title">Type</dt>
+            <dd class="card__info-description">${character.type}</dd>
+            <dt class="card__info-title">Occurrences</dt>
+            <dd class="card__info-description">${character.episode.length}</dd>
+          </dl>
+        </div>
+      </li>
+      `;
+      pagination.innerHTML = ""
+      cardContainer.insertAdjacentHTML("beforeEnd", createCard);
+    });
+  }
+  catch (error) {
+    console.log(error);
+  }
 }
 fetchCharacters(url);
-
